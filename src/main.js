04315258803bc53647cd84ce53854d6fbc14647d
Nirlvy <1240629470@qq.com>
2023-01-06 20:50:32 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import "element-plus/dist/index.css";
 import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 
 import "./assets/main.css";
+import axios from "axios";
 
 const app = createApp(App);
 
 app.use(ElementPlus, { locale: zhCn });
+app.config.globalProperties.$axios = axios;
 
 app.mount("#app");
